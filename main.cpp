7a59f3c2d69e8cab653bf5c09f95e905502618c0
GoingJack <1312159064@qq.com>
2019-06-26 19:33:22 +0800

    01_first_commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include "login.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication::addLibraryPath("./plugins");
+    QApplication a(argc, argv);
+    Login w;
+    w.show();
+
+
+    return a.exec();
+}
